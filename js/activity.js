92385bc9fb0b3e37622058b891a21d76e4e1e1de
S-kus <saumyakus@gmail.com>
2022-03-09 01:02:35 +0530

    completed step-6

--- js/activity.js
+++ js/activity.js
@@ -17,12 +17,14 @@ var app = new Vue({
 		currentenv: null,
 		pawns: [],
 		SugarL10n: null,
+		SugarPresence: null,
 		l10n: {
 			stringAddPawn: '',
 		}
 	},
 	mounted: function () {
 		this.SugarL10n = this.$refs.SugarL10n;
+		this.SugarPresence = this.$refs.SugarPresence;
 	},
 	methods: {
 		initialized: function () {
@@ -36,7 +38,41 @@ var app = new Vue({
 		},
 		onAddClick: function () {
 			this.pawns.push(this.currentenv.user.colorvalue);
-			this.displayText = this.SugarL10n.get("Played", {name: this.currentenv.user.name });
+			this.displayText = this.SugarL10n.get("Played", { name: this.currentenv.user.name });
+		
+			if (this.SugarPresence.isShared()) {
+				var message = {
+					user: this.SugarPresence.getUserInfo(),
+					content: {
+						action: 'update',
+						data: this.currentenv.user.colorvalue
+					}
+				}
+				this.SugarPresence.sendMessage(message);
+			}
+		},
+		onNetworkDataReceived: function(msg) {
+			switch (msg.content.action) {
+				case 'init':
+					this.pawns = msg.content.data;
+					break;
+				case 'update':
+					this.pawns.push(msg.content.data);
+					this.displayText = this.SugarL10n.get("Played", { name: msg.user.name });
+					break;
+			}
+		},
+		onNetworkUserChanged: function(msg) {
+			// Handling only by the host
+			if (this.SugarPresence.isHost) {
+				this.SugarPresence.sendMessage({
+					user: this.SugarPresence.getUserInfo(),
+					content: {
+						action: 'init',
+						data: this.pawns
+					}
+				});
+			}
 		},
 		onStop: function () {
 			// Save current pawns in Journal on Stop
@@ -48,12 +84,13 @@ var app = new Vue({
 		onJournalNewInstance: function() {
 			console.log("New instance");
 		},
-		
 		onJournalDataLoaded: function (data, metadata) {
 			console.log("Existing instance");
 			this.pawns = data.pawns;
 		},
-		
+		onJournalSharedInstance: function() {
+			console.log("Shared instance");
+		},
 		onJournalLoadError: function(error) {
 			console.log("Error loading from journal");
 		},
